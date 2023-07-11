f7e6770071dec74fc29cddf52661870b09bc2870
Steve Jeong <steve@how2flow.net>
2023-07-11 15:45:32 +0900

    update submodule version 3.15.3
    Signed-off-by: Steve Jeong <steve@how2flow.net>

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ _odroid_wiringpi = Extension(
 
 setup(
     name = 'odroid_wiringpi',
-    version = '3.15.2',
+    version = '3.15.3',
     ext_modules = [ _odroid_wiringpi ],
     py_modules = ["odroid_wiringpi"],
     install_requires=[],
